bf6ce145d8707629df15d05bacbaab9e9bb9b608
Manhduc13 <manhduc130702@gmail.com>
2024-11-20 16:20:17 +0700

    Modify main class

--- src/main/java/com/nguyenducmanh/Main.java
+++ src/main/java/com/nguyenducmanh/Main.java
@@ -3,6 +3,5 @@ package com.nguyenducmanh;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("HELLO WORLD!");
     }
 }
